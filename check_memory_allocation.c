ca68f22f72ecfcba6e561d16b3d0bdc6a881f8cc
koechMR <briankipkoech328@gmail.com>
2023-05-23 11:54:22 +0300

    check memory

--- check_memory_allocation.c
+++ check_memory_allocation.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+* check_bufcopy - function that checks for buffer copy
+*@bufcopy: first parameter to be checked
+*@buf: this is the pointer to buf
+*Return 0 on success
+*/
 
 void check_bufcopy(char *bufcopy, char *buf)
 {
@@ -9,6 +15,13 @@ void check_bufcopy(char *bufcopy, char *buf)
 		exit(1);
 	}
 }
+/**
+* check_tokensArray - this function checks memory allocation for the tokenarray
+* was successful
+*@tokensArray: first parameter of double pointer to tokenarray
+*@buf:  pointer to a character array buf
+*Return 0 on success
+*/
 
 
 void check_tokensArray(char **tokensArray, char *buf)
@@ -20,6 +33,12 @@ void check_tokensArray(char **tokensArray, char *buf)
 		exit(1);
 	}
 }
+/**
+*check_path_copy - this function checks the path to copy
+*@path_copy: pointer to a character array path copy
+*Return: return NULL if the path_copy is NULL,
+* otherwise return the path_copy pointer
+*/
 
 char *check_path_copy(char *path_copy)
 {
