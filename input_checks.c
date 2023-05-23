67d836d1aa1dba7e48a71f5290b81535b974769e
koechMR <briankipkoech328@gmail.com>
2023-05-23 12:03:00 +0300

    input checks

--- input_checks.c
+++ input_checks.c
@@ -1,7 +1,17 @@
 #include "main.h"
+/**
+ *check_stream - function that checks and opens a stream file
+ *     based on the command-line arguments
+ *@ac: number of command line arguments
+ *@av: command line arguments
+ *@stream: pointer to a FILE structure representing the stream
+ *Return: returns the opened stream file or stdin if no av is provided
+ */
+
 
 FILE *check_stream(int ac, char **av, FILE *stream)
 {
+
 	if (ac > 1)
 	{
 		stream = fopen(av[1], "r");
@@ -14,11 +24,16 @@ FILE *check_stream(int ac, char **av, FILE *stream)
 	}
 	else
 	{
-		stream = stdin;
-		return (stream);
+			stream = stdin;
+			return (stream);
 	}
 }
-
+/**
+*check_EOF - funuction that checks for the end-of-file
+*@readbytes: number of bytes read
+*@buf: pointer to a character array representing the buffer
+*Return 0 on succes
+*/
 void check_EOF(ssize_t readbytes, char *buf)
 {
 	if (readbytes == EOF || readbytes == -1)
@@ -27,6 +42,11 @@ void check_EOF(ssize_t readbytes, char *buf)
 		exit(0);
 	}
 }
+/**
+ *check_exit - fuunction to check exit
+ *@buf: char buf to be passed
+ *Return 0 on success
+ */
 
 void check_exit(char *buf)
 {
@@ -36,6 +56,10 @@ void check_exit(char *buf)
 		exit(0);
 	}
 }
+/**
+ *check_env - function to check the env
+ *@buf: char buf parameter to be checked
+ */
 
 void check_env(char *buf)
 {
