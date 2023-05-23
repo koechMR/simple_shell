8c135b88056fbedea3c4b167620dce3d0cb3327c
koechMR <briankipkoech328@gmail.com>
2023-05-23 11:56:43 +0300

    free memory

--- free_memory.c
+++ free_memory.c
@@ -1,4 +1,9 @@
 #include "main.h"
+/**
+* free_tokensArray - function that frees the token array
+*@tokensArray: char parameter to the token array
+*Return 0 on success
+*/
 
 void free_tokensArray(char **tokensArray)
 {
