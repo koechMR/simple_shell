b7c5c01ce2ffa6044759c1e2610bd0a78b17f9a5
koechMR <briankipkoech328@gmail.com>
2023-05-23 12:04:01 +0300

    split the command

--- split_command.c
+++ split_command.c
@@ -1,4 +1,17 @@
 #include "main.h"
+/**
+* tokenization - This function performs tokenization of a string using
+*  a specified delimiter.
+* @buf: Pointer to a character array representing the input
+* string to be tokenized.
+* @tokensArray: Double pointer to a char array representing the
+*  array to store the tokens.
+* @delimeter: Pointer to a constant char array representing the
+* delimiter used for tokenization.
+* Return: Returns the tokensArray after tokenization or
+*        NULL if memory allocation fails.
+*/
+
 
 char **tokenization(char *buf, char **tokensArray, const char *delimeter)
 {
